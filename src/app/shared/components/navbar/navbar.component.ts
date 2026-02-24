bc45504b6b44da10c83e324291712c4fb9fe4844
Nitha Sukprasert <nitha_suk@banpunext.com>
2026-02-24 13:57:37 +0700

    adjust navbar mobile menu ui

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CartService } from '../../../core/services/cart.service';
 
@@ -14,7 +14,10 @@ export class NavbarComponent implements OnInit, OnDestroy {
   menuClosing = false;
   private cartSub!: Subscription;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private elRef: ElementRef
+  ) {}
 
   ngOnInit(): void {
     this.cartSub = this.cartService.cart$.subscribe(() => {
@@ -44,6 +47,22 @@ export class NavbarComponent implements OnInit, OnDestroy {
     if (this.menuClosing && event.target === event.currentTarget) {
       this.mobileMenuOpen = false;
       this.menuClosing = false;
+    }
+  }
+
+  /** Close the mobile menu when the user clicks outside the navbar. */
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.mobileMenuOpen && !this.elRef.nativeElement.contains(event.target)) {
+      this.closeMobileMenu();
+    }
+  }
+
+  /** Close the mobile menu on Escape key press. */
+  @HostListener('document:keydown.escape')
+  onEscapeKey(): void {
+    if (this.mobileMenuOpen) {
+      this.closeMobileMenu();
     }
   }
 }
